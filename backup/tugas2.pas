c3cf11c070bace6dd4a74a29ebf9d3be58a2db72
FeriAndika-hub <feri14andika@gmail.com>
2022-11-03 13:54:27 +0700

    feat: add Load Procedure

--- backup/tugas2.pas
+++ backup/tugas2.pas
@@ -5,7 +5,8 @@ unit tugas2;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
+  ExtDlgs;
 
 type
 
@@ -14,6 +15,8 @@ type
   TForm1 = class(TForm)
     btnLoad: TButton;
     imgSrc: TImage;
+    OpenPictureDialog1: TOpenPictureDialog;
+    procedure btnLoadClick(Sender: TObject);
   private
 
   public
@@ -26,6 +29,49 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+var
+  bitmapR, bitmapG, bitmapB, BitmapGray, BitmapBiner : array[0..1000, 0..1000] of integer;
+  histo : array[0..255] of integer;
+
+procedure TForm1.btnLoadClick(Sender: TObject);
+var
+  i, j, R, G, B, gray : integer;
+begin
+  if (OpenPictureDialog1.Execute) then
+  begin
+    imgSrc.Picture.LoadFromFile(OpenPictureDialog1.FileName);
+  end;
+  for j:=0 to imgSrc.Height-1 do
+  begin
+    for i:=0 to imgSrc.Width-1 do
+    begin
+      //mengambil nilai RGB
+      R := GetRValue(imgSrc.Canvas.Pixels[i,j]);
+      G := GetGValue(imgSrc.Canvas.Pixels[i,j]);
+      B := GetBValue(imgSrc.Canvas.Pixels[i,j]);
+      gray := (R + G + B) div 3;
+      bitmapR[i,j] := R;
+      bitmapG[i,j] := G;
+      bitmapB[i,j] := B;
+      bitmapGray[i,j] := gray;
+      if gray>127 then
+        BitmapBiner[i,j] := 1
+      else
+        BitmapBiner[i,j] := 0;
+    end;
+  end;
+
+  for j:= 0  to imgSrc.Height-1 do
+  begin
+    for i := 0 to imgSrc.Width-1 do
+    begin
+      inc(histo[bitmapGray[i,j]]);
+    end;
+  end;
+end;
 
 end.
 
--- tugas2.pas
+++ tugas2.pas
@@ -16,6 +16,7 @@ type
     btnLoad: TButton;
     imgSrc: TImage;
     OpenPictureDialog1: TOpenPictureDialog;
+    procedure btnLoadClick(Sender: TObject);
   private
 
   public
@@ -28,6 +29,52 @@ var
 implementation
 
 {$R *.lfm}
+
+uses
+  windows, math;
+
+{ TForm1 }
+
+var
+  bitmapR, bitmapG, bitmapB, BitmapGray, BitmapBiner : array[0..1000, 0..1000] of integer;
+  histo : array[0..255] of integer;
+
+procedure TForm1.btnLoadClick(Sender: TObject);
+var
+  i, j, R, G, B, gray : integer;
+begin
+  if (OpenPictureDialog1.Execute) then
+  begin
+    imgSrc.Picture.LoadFromFile(OpenPictureDialog1.FileName);
+  end;
+  for j:=0 to imgSrc.Height-1 do
+  begin
+    for i:=0 to imgSrc.Width-1 do
+    begin
+      //mengambil nilai RGB
+      R := GetRValue(imgSrc.Canvas.Pixels[i,j]);
+      G := GetGValue(imgSrc.Canvas.Pixels[i,j]);
+      B := GetBValue(imgSrc.Canvas.Pixels[i,j]);
+      gray := (R + G + B) div 3;
+      bitmapR[i,j] := R;
+      bitmapG[i,j] := G;
+      bitmapB[i,j] := B;
+      bitmapGray[i,j] := gray;
+      if gray>127 then
+        BitmapBiner[i,j] := 1
+      else
+        BitmapBiner[i,j] := 0;
+    end;
+  end;
+
+  for j:= 0  to imgSrc.Height-1 do
+  begin
+    for i := 0 to imgSrc.Width-1 do
+    begin
+      inc(histo[bitmapGray[i,j]]);
+    end;
+  end;
+end;
 
 end.
 
